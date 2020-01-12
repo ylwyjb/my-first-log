de93c2d8fbfca566e8b22787950f57817290f7ac
YWJ <lyywbj@protonmail.com>
2020-01-12 12:21:07 +0800

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):    #- 这行是用来定义我们的模型 (这是一个 对象).
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
